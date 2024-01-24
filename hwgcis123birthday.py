517843fae451aa1e8f036c4b65c02235bbf6a3cf
Youssef-marak <145011978+Youssef-marak@users.noreply.github.com>
2024-01-24 10:06:07 +0400

    Add files via upload

---
+++ hwgcis123birthday.py
@@ -0,0 +1,10 @@
+
+name=input("please enter your name")
+birthmonth= input("please enter your birthmonth")
+birthday_of_the_month=input("please enter your day of birth")
+birthyear=input("please enter your birthyear")
+
+print(name,"your birthday is on",birthmonth,birthday_of_the_month,birthyear)
+
+
+
